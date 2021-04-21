ac7f0a707c42a5050fc32b45775ea4230609757d
LukaszPodlipski <lukaszpodlipskikontakt@gmail.com>
2021-04-21 11:56:03 +0200

    update

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,160 @@
 const burger = document.querySelector(".hamburger");
-const nav = document.querySelector(".mobilenavi");
-const navTR = document.querySelector(".mobilenaviTR");
+const nav = document.getElementById("mobilenavi");
 const mobilelogo = document.querySelector(".logo");
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
+burger.addEventListener("click", () => {
+  if (screen.width <= 320) {
+    if (nav.style.left === "50%") {
+      mobilelogo.style.marginRight = "0px";
+      enableScrolling();
+      nav.style.left = "150%";
+      burger.innerHTML = `<i class="fas fa-bars"></i>`;
+      mobilelogo.style.marginRight = "0px";
+    } else {
+      mobilelogo.style.marginRight = "0px";
+      burger.innerHTML = `<i class="fas fa-times"></i>`;
+      mobilelogo.style.marginRight = "0px";
+      nav.style.left = "50%";
+      disableScrolling();
+    }
+  } else if (screen.width < 380) {
+    if (nav.style.left === "50%") {
+      mobilelogo.style.marginRight = "0px";
+      enableScrolling();
+      nav.style.left = "150%";
+      burger.innerHTML = `<i class="fas fa-bars"></i>`;
+      mobilelogo.style.marginRight = "0px";
+    } else {
+      mobilelogo.style.marginRight = "0px";
+      burger.innerHTML = `<i class="fas fa-times"></i>`;
+      mobilelogo.style.marginRight = "5px";
+      nav.style.left = "50%";
+      disableScrolling();
+    }
+  } else if (screen.width < 900) {
+    if (nav.style.left === "50%") {
+      enableScrolling();
+      nav.style.left = "150%";
+      burger.innerHTML = `<i class="fas fa-bars"></i>`;
+      mobilelogo.style.marginRight = "0px";
+    } else {
+      burger.innerHTML = `<i class="fas fa-times"></i>`;
+      mobilelogo.style.marginRight = "10px";
+      nav.style.left = "50%";
+      disableScrolling();
+    }
+  } else {
+    if (nav.style.left === "50%") {
+      enableScrolling();
+      nav.style.left = "150%";
+      burger.innerHTML = `<i class="fas fa-bars"></i>`;
+      mobilelogo.style.marginRight = "0px";
+    } else {
+      burger.innerHTML = `<i class="fas fa-times"></i>`;
+      mobilelogo.style.marginRight = "10px";
+      nav.style.left = "50%";
+      disableScrolling();
+    }
+  }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+if (screen.width <= 900) {
+  $(window).scroll(function () {
+    $(".circle").css({
+      "margin-top": -$(window).scrollTop() / 2.3 + "px",
+    });
+  });
+}
+/////////////////////////////////
+
+if (screen.width < 500 && screen.width > 414) {
+  $(window).scroll(function () {
+    $(".circle").css({
+      "margin-top": -$(window).scrollTop() / 3 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 2.6 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+if (screen.width < 415 && screen.width > 375) {
+  $(window).scroll(function () {
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 2 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+if (screen.width < 376 && screen.height > 700) {
+  $(window).scroll(function () {
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 2.4 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+if (screen.width < 376 && screen.height < 700) {
+  $(window).scroll(function () {
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 1.5 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+if (screen.width < 361) {
+  $(window).scroll(function () {
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 1 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+
+if (screen.width < 415 && screen.width > 375 && screen.height < 740) {
+  $(window).scroll(function () {
+    $(".circle").css({
+      "margin-top": -$(window).scrollTop() / 2.3 + "px",
+    });
+    $(".circleTR").css({
+      "margin-top": -$(window).scrollTop() / 1.5 + "px",
+      // "margin-left": -$(window).scrollLeft() / 5 + "px",
+    });
+  });
+}
+/////////////////////////////////
+
+if (screen.width < 350) {
+  $(window).scroll(function () {
+    $(".circle").css({
+      "margin-top": -$(window).scrollTop() / 1.9 + "px",
+    });
+  });
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function disableScrolling() {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
+}
+
+function enableScrolling() {
+  window.onscroll = function () {};
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
 function slider(path) {
   document.querySelector(".mainimg").src = path;
 }
@@ -20,93 +172,4 @@ function changeColors(color, color2, textContent) {
   boldtext.style.color = color;
   circle.style.backgroundColor = color;
 }
-
-if (screen.width < 1481 && screen.width > 900 && screen.height > 1000) {
-  if (screen.width < 500) {
-    $(window).scroll(function () {
-      $(".circle").css({
-        "margin-top": -$(window).scrollTop() / 3 + "px",
-        // "margin-left": -$(window).scrollLeft() / 5 + "px",
-      });
-    });
-  }
-  burger.addEventListener("click", () => {
-    if (nav.style.left === "50%") {
-      enableScrolling();
-      nav.style.left = "150%";
-      burger.innerHTML = `<i class="fas fa-bars"></i>`;
-      mobilelogo.style.marginRight = "0px";
-    } else {
-      burger.innerHTML = `<i class="fas fa-times"></i>`;
-      mobilelogo.style.marginRight = "5px";
-      nav.style.left = "50%";
-      disableScrolling();
-    }
-  });
-}
-
-if (screen.width <= 900) {
-  $(window).scroll(function () {
-    $(".circle").css({
-      "margin-top": -$(window).scrollTop() / 2.3 + "px",
-    });
-  });
-  if (screen.width < 500) {
-    $(window).scroll(function () {
-      $(".circle").css({
-        "margin-top": -$(window).scrollTop() / 3 + "px",
-        // "margin-left": -$(window).scrollLeft() / 5 + "px",
-      });
-    });
-  }
-  burger.addEventListener("click", () => {
-    if (nav.style.left === "50%") {
-      enableScrolling();
-      nav.style.left = "150%";
-      burger.innerHTML = `<i class="fas fa-bars"></i>`;
-      mobilelogo.style.marginRight = "0px";
-    } else {
-      burger.innerHTML = `<i class="fas fa-times"></i>`;
-      mobilelogo.style.marginRight = "5px";
-      nav.style.left = "50%";
-      disableScrolling();
-    }
-  });
-}
-
-if (screen.width < 380) {
-  burger.addEventListener("click", () => {
-    if (nav.style.left === "50%") {
-      mobilelogo.style.marginRight = "0px";
-    } else {
-      mobilelogo.style.marginRight = "0px";
-    }
-  });
-}
-
-if (screen.width < 415 && screen.height < 740) {
-  $(window).scroll(function () {
-    $(".circle").css({
-      "margin-top": -$(window).scrollTop() / 2.3 + "px",
-    });
-  });
-}
-if (screen.width < 350) {
-  $(window).scroll(function () {
-    $(".circle").css({
-      "margin-top": -$(window).scrollTop() / 1.9 + "px",
-    });
-  });
-}
-
-function disableScrolling() {
-  var x = window.scrollX;
-  var y = window.scrollY;
-  window.onscroll = function () {
-    window.scrollTo(x, y);
-  };
-}
-
-function enableScrolling() {
-  window.onscroll = function () {};
-}
+///////////////////////////////////////////////////////////////////////////////////////////////////
